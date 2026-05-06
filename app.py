6575c37311084a41ab7bc683d1f8893d6ec9c987
cedricbedu <cedric.bedu@gmail.com>
2026-05-06 17:34:58 +0200

    Fond d'ecran + systeme reco

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
 import streamlit as st
 from recommandation import film_reco, df_encode
+import base64
+
+with open("assets/style.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+def get_base64(fichier):
+    with open(fichier, "rb") as f:
+        return base64.b64encode(f.read()).decode()
+    
+img = get_base64("assets/Fond_ecran_site.png")
+
+st.markdown(f"""
+            <style>
+            .stApp {{
+            background-image : url("data:image/png;base64, {img}");
+            background-size : cover;
+            background-position : center;
+            }}
+            </style>
+            """, unsafe_allow_html=True)
+
 
 st.title("Système de recommandation de films")
+
+df_encode['titre_note'] = df_encode['title'] + " " + df_encode['averageRating'].round(1).astype(str) + "⭐"
 
 film_choisi = st.selectbox(
     "Choisis un film :",
